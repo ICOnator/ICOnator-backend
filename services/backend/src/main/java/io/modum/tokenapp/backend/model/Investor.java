e94984117fc004cb9ee2be0775f6b9d0458624e8
Sebastian Stephan <sebastian@stephan.li>
2017-08-03 17:12:04 +0200

    Use sequence for Investor for payin address lookup

--- services/backend/src/main/java/io/modum/tokenapp/backend/model/Investor.java
+++ services/backend/src/main/java/io/modum/tokenapp/backend/model/Investor.java
@@ -11,7 +11,7 @@ import static javax.persistence.TemporalType.TIMESTAMP;
 public class Investor {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "id", nullable = false)
     private long id;
 
