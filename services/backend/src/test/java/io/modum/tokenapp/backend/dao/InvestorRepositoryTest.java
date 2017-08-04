741ea49dbe572628df2a1a38a45dfae02898055d
Sebastian Stephan <sebastian@stephan.li>
2017-08-04 12:57:03 +0200

    Fixed: Failing tests due to different exception. See JIRA TOK-49.

--- services/backend/src/test/java/io/modum/tokenapp/backend/dao/InvestorRepositoryTest.java
+++ services/backend/src/test/java/io/modum/tokenapp/backend/dao/InvestorRepositoryTest.java
@@ -1,11 +1,12 @@
 package io.modum.tokenapp.backend.dao;
 
 import io.modum.tokenapp.backend.model.Investor;
+import javax.persistence.PersistenceException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Date;
@@ -21,6 +22,9 @@ public class InvestorRepositoryTest {
 
     @Autowired
     private InvestorRepository investorRepository;
+
+    @Autowired
+    private TestEntityManager entityManager;
 
     @Test
     public void testInsert() {
@@ -38,7 +42,7 @@ public class InvestorRepositoryTest {
         assertTrue(oInvestor.isPresent() && oInvestor.get().getEmailConfirmationToken().equals(randomUUID));
     }
 
-    @Test(expected = DataIntegrityViolationException.class)
+    @Test(expected = PersistenceException.class)
     public void testEmailConfirmationTokenUniqueConstraint() {
         String randomUUID = UUID.randomUUID().toString();
         Investor i1 = new Investor().setCreationDate(new Date())
@@ -49,9 +53,10 @@ public class InvestorRepositoryTest {
                                     .setEmail("test2@test2.com")
                                     .setEmailConfirmationToken(randomUUID);
         investorRepository.save(i2);
+        entityManager.flush();
     }
 
-    @Test(expected = DataIntegrityViolationException.class)
+    @Test(expected = PersistenceException.class)
     public void testEmailUniqueConstraint() {
         Investor i1 = new Investor().setCreationDate(new Date())
                                     .setEmail("test@test.com")
@@ -61,6 +66,7 @@ public class InvestorRepositoryTest {
                                     .setEmail("test@test.com")
                                     .setEmailConfirmationToken(UUID.randomUUID().toString());
         investorRepository.save(i2);
+        entityManager.flush();
     }
 
 
