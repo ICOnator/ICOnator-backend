8fd6e967b8c8a36a3792eea83de6b5c529c32c93
Guilherme Sperb Machado <gsm@machados.org>
2017-08-09 20:29:44 +0200

    Revert: Remove QR Code generation

--- services/backend/src/main/java/io/modum/tokenapp/backend/service/MailContentBuilder.java
+++ services/backend/src/main/java/io/modum/tokenapp/backend/service/MailContentBuilder.java
@@ -1,11 +1,14 @@
 package io.modum.tokenapp.backend.service;
 
 import io.modum.tokenapp.backend.model.Investor;
+import net.glxn.qrgen.core.image.ImageType;
+import net.glxn.qrgen.javase.QRCode;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.InputStreamSource;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -78,6 +81,24 @@ public class MailContentBuilder {
                 final InputStreamSource modumLogoImage =
                         new ByteArrayResource(IOUtils.toByteArray(this.getClass().getResourceAsStream("/static/images/modum_logo.png")));
                 oMessage.get().addInline("modumLogo", modumLogoImage, "image/png");
+
+                // payInEtherAddress:
+                ByteArrayOutputStream payInEtherAddressQRCodeStream = QRCode
+                        .from(addressService.getEthereumAddressFromPublicKey(oInvestor.get().getPayInEtherPublicKey()))
+                        .to(ImageType.PNG)
+                        .withSize(265, 200)
+                        .stream();
+                final ByteArrayResource payInEtherAddressQRCode = new ByteArrayResource(payInEtherAddressQRCodeStream.toByteArray());
+                oMessage.get().addInline("payInEtherAddressQRCode", payInEtherAddressQRCode, "image/png");
+
+                // payInBitcoinAddress:
+                ByteArrayOutputStream payInBitcoinAddressQRCodeStream = QRCode
+                        .from(addressService.getBitcoinAddressFromPublicKey(oInvestor.get().getPayInBitcoinPublicKey()))
+                        .to(ImageType.PNG)
+                        .withSize(265, 200)
+                        .stream();
+                final ByteArrayResource payInBitcoinQRCodeImage = new ByteArrayResource(payInBitcoinAddressQRCodeStream.toByteArray());
+                oMessage.get().addInline("payInBitcoinAddressQRCode", payInBitcoinQRCodeImage, "image/png");
 
             } catch (MessagingException e) {
                 LOG.error("Error to add inline images to the message.");
