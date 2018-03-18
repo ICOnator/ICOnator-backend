65d5485a430691e7a828eae19cdf0a246da6fc14
Guilherme Sperb Machado <gsm@machados.org>
2018-03-18 13:07:27 +0100

    Fix BitcoinKeyGeneratorTest.

--- commons/bitcoin/src/test/java/io/iconator/commons/bitcoin/BitcoinKeyGeneratorTest.java
+++ commons/bitcoin/src/test/java/io/iconator/commons/bitcoin/BitcoinKeyGeneratorTest.java
@@ -1,11 +1,15 @@
 package io.iconator.commons.bitcoin;
 
+import io.iconator.commons.bitcoin.config.BitcoinConfig;
 import io.iconator.commons.model.Keys;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
@@ -13,15 +17,20 @@ import java.io.IOException;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {BitcoinConfig.class})
+@TestPropertySource("classpath:application-test.properties")
 public class BitcoinKeyGeneratorTest {
 
     private static final Logger LOG = LoggerFactory.getLogger(BitcoinKeyGeneratorTest.class);
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
+    @Autowired
+    private BitcoinConfig bitcoinConfig;
+
     @Test
     public void testGenerateAndValidateAddress() throws IOException {
-        BitcoinKeyGenerator bitcoinKeyGenerator = new BitcoinKeyGenerator("testnet");
+        BitcoinKeyGenerator bitcoinKeyGenerator = new BitcoinKeyGenerator(bitcoinConfig);
         Keys keys = bitcoinKeyGenerator.getKeys();
         String jsonKeys = objectMapper.writer().writeValueAsString(keys);
         LOG.info(jsonKeys);
