7e2e9a71c821915cb20b6f6b8fe767954d335ae4
Jonny Burger <jonathanburger11@gmail.com>
2018-03-20 11:29:24 +0100

    Narrow down CORS to the correct domains

--- services/core/src/main/java/io/iconator/backend/integration/CorsConfiguration.java
+++ services/core/src/main/java/io/iconator/backend/integration/CorsConfiguration.java
@@ -4,14 +4,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
-@Profile({"!prod"})
 public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Value("${io.iconator.backend.frontendUrl}")
+    private String frontendUrl;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*").allowedOrigins("*").allowedOrigins("*");
+        registry.addMapping("/**").allowedMethods("*").allowedOrigins(this.frontendUrl);
     }
 
 }
