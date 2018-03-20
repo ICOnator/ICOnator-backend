53856060c1a0cf8ffeb26de4ed81c852bd92be14
Guilherme Sperb Machado <gsm@machados.org>
2018-03-20 01:56:09 +0100

    Fix CorsConfiguration to allow everything for dev purposes [2].

--- services/core/src/main/java/io/iconator/backend/integration/CorsConfiguration.java
+++ services/core/src/main/java/io/iconator/backend/integration/CorsConfiguration.java
@@ -4,16 +4,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 @Profile({"!prod"})
 public class CorsConfiguration implements WebMvcConfigurer {
-    @Value("${io.iconator.backend.frontendUrl}")
-    private String frontendUrl;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*").allowedOrigins("*").allowedOrigins(this.frontendUrl);
+        registry.addMapping("/**").allowedMethods("*").allowedOrigins("*").allowedOrigins("*");
     }
+
 }
