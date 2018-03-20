e456688c7e72eb7c3091c07690bf3c17fe493aae
Guilherme Sperb Machado <gsm@machados.org>
2018-03-20 19:31:53 +0100

    Instead of adding the frontendUrl as the CORS allowed origin, give the possibility to specify multiple URLs (comma separated) as a configuration param (env var).

--- services/core/src/main/java/io/iconator/backend/integration/CorsConfiguration.java
+++ services/core/src/main/java/io/iconator/backend/integration/CorsConfiguration.java
@@ -1,20 +1,19 @@
 package io.iconator.backend.integration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
 
-    @Value("${io.iconator.backend.frontendUrl}")
-    private String frontendUrl;
+    @Value("${io.iconator.backend.cors.urls}")
+    private String[] corsUrls;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*").allowedOrigins(frontendUrl.substring(0, frontendUrl.length() - 1));
+        registry.addMapping("/**").allowedMethods("*").allowedOrigins(this.corsUrls);
     }
 
 }
