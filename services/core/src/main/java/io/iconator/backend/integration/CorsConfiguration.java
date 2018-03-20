c3c7f0eaee86e78a90d63cc81d4e2ef9104d8bdb
Jonny Burger <jonathanburger11@gmail.com>
2018-03-20 11:54:02 +0100

    Remove trailing slash from CORS origin

--- services/core/src/main/java/io/iconator/backend/integration/CorsConfiguration.java
+++ services/core/src/main/java/io/iconator/backend/integration/CorsConfiguration.java
@@ -14,7 +14,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*").allowedOrigins(this.frontendUrl);
+        registry.addMapping("/**").allowedMethods("*").allowedOrigins(frontendUrl.substring(0, frontendUrl.length() - 1));
     }
 
 }
