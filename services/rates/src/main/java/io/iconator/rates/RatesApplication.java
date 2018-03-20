f5ccfb0a98639369fc4411598a2431114f336c89
Guilherme Sperb Machado <gsm@machados.org>
2018-03-20 22:44:38 +0100

    Remove unused code portion from RatesApplication.

--- services/rates/src/main/java/io/iconator/rates/RatesApplication.java
+++ services/rates/src/main/java/io/iconator/rates/RatesApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import static org.springframework.boot.SpringApplication.run;
@@ -27,29 +26,4 @@ public class RatesApplication {
         }
     }
 
-//    public void run(String... args) throws Exception {
-//        if (options.getExportFile() != null) {
-//            File file = new File(options.getExportFile());
-//            FileWriter fileWriter = new FileWriter(file);
-//            PrintWriter printWriter = new PrintWriter(fileWriter);
-//            for (ExchangeRate p : repository.findAllByOrderByCreationDate()) {
-//                printWriter.print(p.getCreationDate().getTime());
-//                printWriter.print(",");
-//                printWriter.print(p.getBlockNrBtc());
-//                printWriter.print(",");
-//                printWriter.print(p.getBlockNrEth());
-//                printWriter.print(",");
-//                printWriter.print(p.getRateBtc());
-//                printWriter.print(",");
-//                printWriter.print(p.getRateBtcBitfinex());
-//                printWriter.print(",");
-//                printWriter.print(p.getRateIotaBitfinex());
-//                printWriter.print(",");
-//                printWriter.print(p.getRateEth());
-//                printWriter.print(",");
-//                printWriter.print(p.getRateEthBitfinex());
-//            }
-//            fileWriter.close();
-//        }
-//    }
 }
