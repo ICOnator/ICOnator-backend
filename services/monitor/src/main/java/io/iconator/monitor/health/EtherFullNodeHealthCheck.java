d0d94c942b8a57f5df97063d645e849d5af2b6ba
Guilherme Sperb Machado <gsm@machados.org>
2018-03-26 23:03:24 +0200

    Adapted the EtherFullNodeHealthCheck class.

--- services/monitor/src/main/java/io/iconator/monitor/health/EtherFullNodeHealthCheck.java
+++ services/monitor/src/main/java/io/iconator/monitor/health/EtherFullNodeHealthCheck.java
@@ -1,12 +1,16 @@
 package io.iconator.monitor.health;
 
-import org.springframework.beans.factory.annotation.Value;
+import io.iconator.monitor.config.MonitorAppConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.net.*;
+import java.net.InetSocketAddress;
+import java.net.MalformedURLException;
+import java.net.Socket;
+import java.net.URL;
 
 /**
  * A health indicator that checks the connection and latency to the Ethereum node
@@ -14,17 +18,16 @@ import java.net.*;
 @Component
 public class EtherFullNodeHealthCheck implements HealthIndicator {
 
-    @Value("${io.iconator.monitor.etherfullnodeurl}")
-    private String etherNodeUrl;
-
-    private URL url;
+    @Autowired
+    private MonitorAppConfig monitorAppConfig;
 
     @Override
     public Health health() {
+        URL url;
         try {
-            url = new URL(etherNodeUrl);
-        } catch(MalformedURLException mue) {
-            return Health.down(mue).withDetail("URL", etherNodeUrl).build();
+            url = new URL(this.monitorAppConfig.getEthereumNodeUrl());
+        } catch (MalformedURLException mue) {
+            return Health.down(mue).withDetail("URL", this.monitorAppConfig.getEthereumNodeUrl()).build();
         }
 
         InetSocketAddress sa = new InetSocketAddress(url.getHost(), url.getPort());
@@ -33,14 +36,14 @@ public class EtherFullNodeHealthCheck implements HealthIndicator {
         long start = System.currentTimeMillis();
         try {
             s.connect(sa, 1000);
-        } catch(Exception e) {
+        } catch (Exception e) {
             return Health.down(e).build();
         }
         long latency = System.currentTimeMillis() - start;
 
         try {
             s.close();
-        } catch(IOException ioe) {
+        } catch (IOException ioe) {
             return Health.down(ioe).build();
         }
 
