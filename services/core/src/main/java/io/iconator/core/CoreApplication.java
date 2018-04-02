a63865eb1beea28b0ac04e8bad25d818178d37f4
tbocek <tom@tomp2p.net>
2018-04-02 16:22:59 +0200

    Generate jar files for service modules as well.

--- services/core/src/main/java/io/iconator/core/CoreApplication.java
+++ services/core/src/main/java/io/iconator/core/CoreApplication.java
@@ -1,6 +1,5 @@
 package io.iconator.core;
 
-import com.google.common.base.Strings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -24,28 +23,12 @@ public class CoreApplication {
 
     public static void main(String[] args) {
         try {
-            startMessageQueue();
             run(CoreApplication.class, args);
         } catch (Throwable t) {
             //ignore silent exception
             if(!t.getClass().toString().endsWith("SilentExitException")) {
                 LOG.error("cannot execute core", t);
             }
-        }
-    }
-
-    /**
-     * Starts the message queue when no profile is set, this means we are local
-     * and we want to start the message queue on this machine. Also, the dev-tools
-     * with Spring Boot, restart the application, thus, we don't want to restart the queue.
-     *
-     * @throws Exception
-     */
-    private static void startMessageQueue() throws Exception {
-        String profile = System.getenv("spring.profiles.active");
-        if(Strings.isNullOrEmpty(profile)
-                && !Thread.currentThread().getName().equals("restartedMain")) {
-            io.iconator.commons.test.utils.BuiltInMessageBroker.start();
         }
     }
 }
