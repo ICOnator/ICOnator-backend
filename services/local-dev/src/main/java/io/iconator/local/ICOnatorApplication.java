d5c2cf3cb63118d83ef4af2f695f52480b08d1ba
tbocek <tom@tomp2p.net>
2018-04-02 23:27:57 +0200

    added property files to start all services locally

--- services/local-dev/src/main/java/io/iconator/local/ICOnatorApplication.java
+++ services/local-dev/src/main/java/io/iconator/local/ICOnatorApplication.java
@@ -6,9 +6,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.PropertySource;
 
 
 @SpringBootApplication
+@PropertySource({
+        "core.application.properties",
+        "email.application.properties",
+        "monitor.application.properties",
+        "rates.application.properties"})
 public class ICOnatorApplication {
     private static final Logger LOG = LoggerFactory.getLogger(ICOnatorApplication.class);
 
