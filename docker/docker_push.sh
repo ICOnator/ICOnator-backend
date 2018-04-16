4cf42fa2fc94a7cf07813670f5c7f69c8fb1827d
Guilherme Sperb Machado <gsm@machados.org>
2018-04-17 01:38:38 +0200

    Add docker service to travis and enable dockerPush.

--- docker/docker_push.sh
+++ docker/docker_push.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-sh gradlew -PdockerVersion=$ICONATOR_DOCKER_VERSION -PdockerImageName=$ICONATOR_DOCKER_IMAGE_NAME docker dockerTag
+sh gradlew -PdockerVersion=$ICONATOR_DOCKER_VERSION -PdockerImageName=$ICONATOR_DOCKER_IMAGE_NAME docker dockerTag dockerPush
