ce795c2f783f84b5425a409f6005c76d9b7bc9fb
Guilherme Sperb Machado <gsm@machados.org>
2018-07-06 18:28:52 +0200

    Add an API to fetch current rates.

---
+++ services/rates/src/main/java/io/iconator/rates/service/exceptions/USDETHFxException.java
@@ -0,0 +1,20 @@
+package io.iconator.rates.service.exceptions;
+
+public class USDETHFxException extends Exception {
+
+    public USDETHFxException(String message) {
+        super(message);
+    }
+
+    public USDETHFxException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public USDETHFxException(Throwable cause) {
+        super(cause);
+    }
+
+    public USDETHFxException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
