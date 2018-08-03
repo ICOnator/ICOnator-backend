ea4ec8c6741f49eeb3a8b40a3518d0f38e634c30
Guilherme Sperb Machado <gsm@machados.org>
2018-08-03 11:26:19 +0200

    Disable session creation for the security-actuator auth module.

--- commons/security-actuator/src/main/java/io/iconator/commons/auth/ActuatorAuthSecurityConfig.java
+++ commons/security-actuator/src/main/java/io/iconator/commons/auth/ActuatorAuthSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -27,7 +28,12 @@ public class ActuatorAuthSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests()
+
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .csrf().disable()
+                .authorizeRequests()
                 .antMatchers("/actuator/**").hasRole(ROLE)
                 .antMatchers("/cloudfoundryapplication/**").hasRole(ROLE)
                 .and()
