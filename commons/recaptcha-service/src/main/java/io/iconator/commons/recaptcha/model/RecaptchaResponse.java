4a044865d7466611b71b9f1cad4cf49a76ba43f1
Guilherme Sperb Machado <gsm@machados.org>
2018-08-10 18:09:17 +0200

    Minor change: add json ignore properties on the Recaptcha responses DTO.

--- commons/recaptcha-service/src/main/java/io/iconator/commons/recaptcha/model/RecaptchaResponse.java
+++ commons/recaptcha-service/src/main/java/io/iconator/commons/recaptcha/model/RecaptchaResponse.java
@@ -1,10 +1,12 @@
 package io.iconator.commons.recaptcha.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.Date;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class RecaptchaResponse {
 
     /**
