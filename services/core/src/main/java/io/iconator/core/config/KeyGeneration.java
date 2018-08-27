3cdaee37b4a100163c9d9b068ac3c064c5d86bc3
Guilherme Sperb Machado <gsm@machados.org>
2018-08-27 23:32:57 +0200

    The key generation with test keys should only be executed for profile "dev".

--- services/core/src/main/java/io/iconator/core/config/KeyGeneration.java
+++ services/core/src/main/java/io/iconator/core/config/KeyGeneration.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.Resource;
 
 import javax.annotation.PostConstruct;
@@ -15,7 +16,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 @Configuration
-//@Profile("dev")
+@Profile("dev")
 public class KeyGeneration {
 
 
