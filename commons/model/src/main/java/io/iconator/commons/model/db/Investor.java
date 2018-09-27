5524a7d9e162ca89b0f276a1af736ec00798538a
Guilherme Sperb Machado <gsm@machados.org>
2018-09-27 18:40:13 +0200

    Minor change (code style).

--- commons/model/src/main/java/io/iconator/commons/model/db/Investor.java
+++ commons/model/src/main/java/io/iconator/commons/model/db/Investor.java
@@ -177,10 +177,12 @@ public class Investor {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o)
+        if (this == o) {
             return true;
-        if (o == null || getClass() != o.getClass())
+        }
+        if (o == null || getClass() != o.getClass()) {
             return false;
+        }
         Investor investor = (Investor) o;
 
         // TODO: is it enough?
