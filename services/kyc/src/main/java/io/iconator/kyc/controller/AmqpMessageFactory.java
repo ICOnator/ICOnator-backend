484ca608f9f512d162dc96ac34806177d28ff093
Simon <simon.mueller@simondo.ch>
2019-01-21 18:55:21 +0100

    updated documentation for kyc module

--- services/kyc/src/main/java/io/iconator/kyc/controller/AmqpMessageFactory.java
+++ services/kyc/src/main/java/io/iconator/kyc/controller/AmqpMessageFactory.java
@@ -8,12 +8,25 @@ import java.net.URI;
 
 import static io.iconator.commons.amqp.model.utils.MessageDTOHelper.build;
 
+/**
+ * Helper class for creating AMQP messages enabling simpler testing
+ */
 public class AmqpMessageFactory {
 
+    /**
+     * @param investor The investor to whom the email should be sent
+     * @param kycUri URI to start the KYC process
+     * @return AMQP message used to send the KYC start email
+     */
     public KycStartEmailMessage makeKycStartEmailMessage(Investor investor, URI kycUri) {
         return new KycStartEmailMessage(build(investor), kycUri.toASCIIString());
     }
 
+    /**
+     * @param investor The investor to whom the email should be sent
+     * @param kycUri URI to start the KYC process
+     * @return AMQP message used to send the KYC reminder email
+     */
     public KycReminderEmailMessage makeKycReminderEmailMessage(Investor investor, URI kycUri) {
         return new KycReminderEmailMessage(build(investor), kycUri.toASCIIString());
     }
